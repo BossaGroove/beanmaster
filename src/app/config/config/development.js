09cabae83517249451703314a8b33ab60bebf2ac
Bossa Wong <bossa@aftership.com>
2014-12-04 13:51:19 +0800

    added binary

--- src/app/config/config/development.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	title: 'beanmaster',
-	keywords: 'nodejs,beanstalkd,console',
-	description: 'nodejs beanstalks console',
-	root: require('path').normalize(__dirname + '/../../../'),
-	port: 3001
-};
--- src/server.js
+++ src/server.js
@@ -2,13 +2,26 @@ var express = require('express');
 var config = require('./app/config/config');
 var app = express();
 
+var args = process.argv,
+	arg = null;
+
+var port = null;
+
+while (arg = args.shift()) {
+	if (arg === '--port' || arg === '-p') {
+		port = args.shift();
+	}
+}
+
+if (!port || isNaN(port)) {
+	port = config.port;
+}
+
 // Bootstrap application settings
 require('./app/config/express')(app);
 
 // Bootstrap routes
 require('./app/config/routes')(app);
 
-app.listen(config.port);
-console.log('Beanmaster started on port ' + config.port);
-
-module.exports = app;
+app.listen(port);
+console.log('Beanmaster started on port ' + port);
