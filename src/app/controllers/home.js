00eb833da0c7725ccbd6ec9e01a6b681a977779c
Bossa Wong <bossa@aftership.com>
2015-03-24 12:49:36 +0800

    show server info

--- src/app/controllers/home.js
+++ src/app/controllers/home.js
@@ -2,7 +2,36 @@
  * Created by Bossa on 2014/11/30.
  */
 
-var BeanstalkConfigManager = require('../../lib/beanstalk_config_manager');
+var BeanstalkConfigManager = require('../../lib/beanstalk_config_manager'),
+	BeanstalkConnectionManager = require('../../lib/beanstalk_connection_manager'),
+	async = require('async'),
+	_ = require('lodash');
+
+/**
+ * get server info
+ * @param configs {Array} - configs
+ * @param callback {function(Object, Array=)} - finish callback
+ */
+function getServerInfo(configs, callback) {
+	async.mapLimit(configs, 5, function(config, flow_callback) {
+		BeanstalkConnectionManager.getConnection(config.host, config.port, function(err, connection) {
+
+			var config_with_detail = _.cloneDeep(config);
+			config_with_detail.server_info = {};
+
+			if (!err) {
+				connection.stats(function(err, results) {
+					config_with_detail.server_info = results;
+					flow_callback(null, config_with_detail);
+				});
+			} else {
+				flow_callback(null, config_with_detail);
+			}
+		});
+	}, function(err, config_with_detail) {
+		callback(err, config_with_detail);
+	});
+}
 
 /**
  *
@@ -13,13 +42,20 @@ var BeanstalkConfigManager = require('../../lib/beanstalk_config_manager');
  */
 exports.servers = function(req, res) {
 
-	BeanstalkConfigManager.getConfig(function(err, config){
-		res.render('home/servers', {
-			page: 'Servers',
-			title: 'Beanmaster',
-			config: config
+	BeanstalkConfigManager.getConfig(function(err, configs) {
+		getServerInfo(configs, function(err, config_with_detail) {
+			if (err || !config_with_detail) {
+				config_with_detail = [];
+			}
+			res.render('home/servers', {
+				page: 'Servers',
+				title: 'Beanmaster',
+				config: config_with_detail
+			});
+
 		});
 	});
+
 };
 
 /**
@@ -37,9 +73,11 @@ exports.addServer = function(req, res) {
 	};
 
 	BeanstalkConfigManager.addConfig(new_config, function(err, new_config) {
-		res.json({
-			err: err,
-			config: new_config
+		getServerInfo(new_config, function(err, config_with_detail) {
+			res.json({
+				err: err,
+				config: config_with_detail
+			});
 		});
 	});
 
@@ -58,9 +96,11 @@ exports.deleteServer = function(req, res) {
 	};
 
 	BeanstalkConfigManager.deleteConfig(config_to_be_deleted, function(err, new_config) {
-		res.json({
-			err: err,
-			config: new_config
+		getServerInfo(new_config, function(err, config_with_detail) {
+			res.json({
+				err: err,
+				config: config_with_detail
+			});
 		});
 	});
 
