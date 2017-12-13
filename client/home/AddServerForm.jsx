3325f65d0d7caed29b180e689bf42f0c010ed460
Bossa <bossagrooveaware@gmail.com>
2017-12-13 18:03:37 +0800

    Add server form utilize redux form

--- client/home/AddServerForm.jsx
+++ client/home/AddServerForm.jsx
@@ -1,38 +1,52 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form';
-import {Alert, Button, FormControl, ControlLabel, Modal, FormGroup, Form, Col} from 'react-bootstrap';
+import { ControlLabel, FormGroup, Form, Col} from 'react-bootstrap';
+import validator from 'validator';
+import _ from 'lodash';
 
-let AddServerForm = () => (
-	<Form horizontal>
-		<FormGroup controlId="name">
+const validate = (values) => {
+	const errors = {};
+
+	if (!values.name || values.name === '') {
+		errors.name = 'Required';
+	}
+
+	if (!values.host || !validator.isURL(values.host || '') && !validator.isIP(values.host || '') && values.host !== 'localhost') {
+		errors.host = 'Host invalid';
+	}
+
+	const port = parseInt(values.port);
+	if (!values.port || !_.isFinite(port) || port <= 0 || port >= 65536 || !values.port.match(/[0-9]+/)) {
+		errors.port = 'Port invalid';
+	}
+
+	return errors;
+};
+
+const renderInput = ({input, className, type, placeholder, meta}) => {
+	return (
+		<FormGroup controlId={input.name} validationState={meta.error && meta.touched?'error':null}>
 			<Col componentClass={ControlLabel} sm={2}>
-				Name
+				Name {meta.error && meta.touched?`* ${meta.error}`:''}
 			</Col>
 			<Col sm={10}>
-				<Field className="form-control" name="name" component="input" type="text" placeholder="My cool beanstalk server" />
+			<input {...input} className={className} type={type} placeholder={placeholder} />
 			</Col>
 		</FormGroup>
-		<FormGroup controlId="host">
-			<Col componentClass={ControlLabel} sm={2}>
-				Host
-			</Col>
-			<Col sm={10}>
-				<Field className="form-control" name="host" component="input" type="text" placeholder="127.0.0.1 / localhost / my-cool-beanstalk-server.com" />
-			</Col>
-		</FormGroup>
-		<FormGroup controlId="port">
-			<Col componentClass={ControlLabel} sm={2}>
-				Port
-			</Col>
-			<Col sm={10}>
-				<Field className="form-control" name="port" component="input" type="number" placeholder="11300, port should be > 1 and < 65536" />
-			</Col>
-		</FormGroup>
+	);
+};
+
+let AddServerForm = (props) => (
+	<Form horizontal onSubmit={props.handleSubmit}>
+		<Field className="form-control" name="name" component={renderInput} type="text" placeholder="My cool beanstalk server" />
+		<Field className="form-control" name="host" component={renderInput} type="text" placeholder="127.0.0.1 / localhost / my-cool-beanstalk-server.com" />
+		<Field className="form-control" name="port" component={renderInput} type="number" placeholder="11300, port should be > 1 and < 65536" />
 	</Form>
 );
 
 AddServerForm = reduxForm({
-	form: 'add_server'
+	form: 'add_server',
+	validate
 })(AddServerForm);
 
 export default AddServerForm;
--- client/home/AddServerModal.jsx
+++ client/home/AddServerModal.jsx
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import validator from 'validator';
-import _ from 'lodash';
 import axios from 'axios';
-import {Alert, Button, FormControl, ControlLabel, Modal, FormGroup, Form, Col} from 'react-bootstrap';
+import {Button, Modal} from 'react-bootstrap';
 import {withRouter} from 'react-router-dom';
 
 import {isBusy, notBusy} from '../actions/busy';
@@ -11,72 +9,20 @@ import {addServer} from '../actions/servers';
 import {hideAddServerModal} from '../actions/addServerModal';
 
 import AddServerForm from './AddServerForm';
+import AddServerFormSubmitButton from './AddServerFormSubmitButton';
 
 import Preloader from '../include/Preloader';
-
-const fields = ['name', 'host', 'port'];
 
 class AddServerModal extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			show: false,
-			errorMessage: ''
+			show: false
 		};
 	}
 
-	handleFormChange(e) {
-		const id = e.target.id;
-		const state = {};
-		state[`form_${id}`] = e.target.value;
-		this.setState(state);
-	}
-
-	getValidationState(id) {
-		return this.getError(id);
-	}
-
-	getError(id, validateEmpty) {
-		const value = this.state[`form_${id}`];
-
-		if (!validateEmpty && _.isUndefined(value)) {
-			return null;
-		}
-
-		switch (id) {
-			case 'name':
-				if (value === '') {
-					return 'error';
-				}
-				break;
-			case 'host':
-				if (!validator.isURL(value || '') && !validator.isIP(value || '') && value !== 'localhost') {
-					return 'error';
-				}
-				break;
-			case 'port':
-				const port = parseInt(value);
-				if (!_.isFinite(port) || port <= 0 || port >= 65536) {
-					return 'error';
-				}
-				break;
-		}
-
-		return null;
-	}
-
 	addServer() {
-		const error = this.getError('name', true) || this.getError('host', true) || this.getError('port', true);
-		if (error) {
-			this.setError('Form invalid');
-			return;
-		}
-
-		this.setState({
-			errorMessage: ''
-		});
-
 		this.props.isBusy();
 
 		AddServerModal
@@ -105,40 +51,12 @@ class AddServerModal extends Component {
 		this.props.hideAddServerModal();
 	}
 
-	setError(errorMessage) {
-		this.setState({
-			errorMessage: errorMessage
-		});
-
-		this.errorTimeout();
-	}
-
-	errorTimeout() {
-		setTimeout(() => {
-			this.setState({
-				errorMessage: ''
-			});
-		}, 5000);
-	}
-
-	static isErrorMessageValid(message) {
-		return (_.isString(message) && !_.isEmpty(message));
-	}
-
 	onSubmit(values) {
-
+		console.log('onSubmit');
+		console.log(values);
 	}
 
 	render() {
-		let alertBox = '';
-		if (AddServerModal.isErrorMessageValid(this.state.errorMessage)) {
-			alertBox = (
-				<Alert bsStyle="warning">
-					{this.state.errorMessage}
-				</Alert>
-			);
-		}
-
 		return (
 			<span>
 				<Modal show={this.props.addServerModal.show} onHide={()=>this.close()}>
@@ -146,39 +64,12 @@ class AddServerModal extends Component {
 						<Modal.Title>Add Server</Modal.Title>
 					</Modal.Header>
 					<Modal.Body>
-						{alertBox}
-						<AddServerForm onSubmit={this.submit} />
-						<Form horizontal>
-							<FormGroup controlId="name" validationState={this.getValidationState('name')}>
-								<Col componentClass={ControlLabel} sm={2}>
-									Name
-								</Col>
-								<Col sm={10}>
-									<FormControl type="text" onChange={this.handleFormChange.bind(this)} placeholder="My cool beanstalk server" />
-								</Col>
-							</FormGroup>
-							<FormGroup controlId="host" validationState={this.getValidationState('host')}>
-								<Col componentClass={ControlLabel} sm={2}>
-									Host
-								</Col>
-								<Col sm={10}>
-									<FormControl type="text" onChange={this.handleFormChange.bind(this)} placeholder="127.0.0.1 / localhost / my-cool-beanstalk-server.com" />
-								</Col>
-							</FormGroup>
-							<FormGroup controlId="port" validationState={this.getValidationState('port')}>
-								<Col componentClass={ControlLabel} sm={2}>
-									Port
-								</Col>
-								<Col sm={10}>
-									<FormControl type="number" onChange={this.handleFormChange.bind(this)} placeholder="11300, port should be > 1 and < 65536" />
-								</Col>
-							</FormGroup>
-						</Form>
+						<AddServerForm onSubmit={this.onSubmit} />
 					</Modal.Body>
 					<Modal.Footer>
 						<Preloader show={this.props.busy} />
 						<Button onClick={()=>this.close()}>Close</Button>
-						<Button bsStyle="primary" onClick={()=>this.addServer()}>Add</Button>
+						<AddServerFormSubmitButton />
 					</Modal.Footer>
 				</Modal>
 			</span>
