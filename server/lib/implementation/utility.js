8a44f3d513b123e814ea79719130d3dbcb8443c1
Bossa <bossagrooveaware@gmail.com>
2018-04-24 22:21:10 +0800

    removed unused functions

--- server/lib/implementation/utility.js
+++ server/lib/implementation/utility.js
@@ -5,29 +5,8 @@
 'use strict';
 
 const path = require('path');
-const validator = require('validator');
-const querystring = require('querystring');
-const _ = require('lodash');
 
 class Utility {
-	/**
-	 * @param {string} host_port
-	 * @returns {*}
-	 */
-	static validateHostPort(host_port) {
-		if (!host_port) {
-			return false;
-		}
-
-		const [host, port] = host_port.split(':');
-
-		if ((!validator.isURL(host) && !validator.isIP(host)) || !validator.isNumeric(port)) {
-			return false;
-		}
-
-		return host_port;
-	}
-
 	static getHomePath() {
 		let beanmaster_home_path = '';
 
@@ -38,40 +17,6 @@ class Utility {
 		}
 
 		return beanmaster_home_path;
-	}
-
-	/**
-	 * @param {Object} location
-	 * @param {String} location.pathname - path name e.g. "/", "/abc", "/abc/", depends on browser exact path
-	 * @param {String} location.search - query string including "?", e.g. "", "?abc=1", "?abc=1&def=2"
-	 * @param {String} inputPath
-	 * @param {Object} query
-	 * @return {String}
-	 */
-	static pushHistory(location, inputPath, query) {
-		// build the path
-		let pathSeparator = '';
-
-		if (location.pathname.substr(location.pathname.length - 1) !== '/') {
-			pathSeparator = '/';
-		}
-
-		let outputPath = `${location.pathname}${pathSeparator}${inputPath}`;
-
-		let cleanQuery = location.search;
-		if (cleanQuery[0] === '?') {
-			cleanQuery = cleanQuery.substr(1, cleanQuery.length - 1);
-		}
-
-		let queryObject = querystring.parse(cleanQuery);
-		queryObject = _.merge(queryObject, query);
-
-		let outputQuery = querystring.stringify(queryObject);
-		if (outputQuery !== '') {
-			outputQuery = `?${outputQuery}`;
-		}
-
-		return `${outputPath}${outputQuery}`;
 	}
 }
 
