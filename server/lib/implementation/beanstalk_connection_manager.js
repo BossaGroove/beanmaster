b0ecce9665eb7203dddd2a871b86fe264fdd728f
Bossa <bossagrooveaware@gmail.com>
2018-04-24 11:32:06 +0800

    beanstalk connection manager become static functions

--- server/lib/implementation/beanstalk_connection_manager.js
+++ server/lib/implementation/beanstalk_connection_manager.js
@@ -1,21 +1,17 @@
-/**
- * Created by Bossa on 2014/10/05.
- */
-
 'use strict';
 
 const Fivebeans = require('fivebeans');
 const bluebird = require('bluebird');
 
 class BeanstalkConnectionManager {
-	async connect(host, port) {
+	static async connect(host, port) {
 		if (!host || !port) {
 			throw new Error('Host or port invalid');
 		}
 
 		const port_int = parseInt(port, 10);
 
-		console.log(`Try to initiate a new beanstalk connection: ${host} / ${port_int}`);
+		// console.log(`Try to initiate a new beanstalk connection: ${host} / ${port_int}`);
 
 		const client = new Fivebeans.client(host, port_int);
 
@@ -27,7 +23,7 @@ class BeanstalkConnectionManager {
 
 		await (new Promise((resolve, reject) => {
 			client.on('connect', () => {
-				console.log(`Connected: ${host} / ${port_int}`);
+				// console.log(`Connected: ${host} / ${port_int}`);
 				resolve();
 			});
 			client.on('error', (err) => {
@@ -39,14 +35,13 @@ class BeanstalkConnectionManager {
 			}, 10000);
 		}));
 
-
 		return client;
 	}
 
-	async closeConnection(connection) {
-		console.log(`Try to end a connection: ${connection.host} / ${connection.port}`);
+	static async closeConnection(connection) {
+		// console.log(`Try to end a connection: ${connection.host} / ${connection.port}`);
 		connection.end();
 	}
 }
 
-module.exports = new BeanstalkConnectionManager();
+module.exports = BeanstalkConnectionManager;
